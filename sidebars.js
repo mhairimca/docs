e47ee2c537c6931aad99505f45b892ea4f03a899
mhairimca <37091375+mhairimca@users.noreply.github.com>
2021-07-06 12:20:18 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -90,7 +90,7 @@ module.exports = {
         },
       ],
     "Events & Press": [
-      "community/community-overview", "community/usingtokens", "community/communityguide", "community/product",
+      "community/community-overview", "community/SuperUMAns" "community/usingtokens", "community/communityguide", "community/product",
       {
         type: "category",
         label: "Events & Press",
